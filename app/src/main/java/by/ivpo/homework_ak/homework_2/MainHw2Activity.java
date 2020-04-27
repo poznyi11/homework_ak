429207129b6077dd7ed6df182dd398e5688c915d
poznyi11 <poznyi11@gmail.com>
2020-04-27 21:53:06 +0300

    start hw_2

---
+++ app/src/main/java/by/ivpo/homework_ak/homework_2/MainHw2Activity.java
@@ -0,0 +1,22 @@
+package by.ivpo.homework_ak.homework_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import by.ivpo.homework_ak.R;
+
+public class MainHw2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_hw2);
+    }
+
+    public void startUserNumbers(View view) {
+    }
+
+    public void startRandomNumbers(View view) {
+    }
+}
