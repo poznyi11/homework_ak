33e00ba3749d1f7ef268d2383eefc6386591ea24
poznyi11 <poznyi11@gmail.com>
2020-04-28 23:02:17 +0300

    Добавлено сохранение данных во View при повороте экрана

--- app/src/main/java/by/ivpo/homework_ak/homework_1/Hw1Task1Vers1Activity.java
+++ app/src/main/java/by/ivpo/homework_ak/homework_1/Hw1Task1Vers1Activity.java
@@ -1,5 +1,6 @@
 package by.ivpo.homework_ak.homework_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,31 +11,78 @@ import by.ivpo.homework_ak.R;
 
 public class Hw1Task1Vers1Activity extends AppCompatActivity {
 
+    final String numberKey = "number";
+    int number = 110;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hw1_task1_vers1);
 
-        setNumber(100);
+        if (savedInstanceState != null) {
+            this.number = savedInstanceState.getInt(numberKey);
+        }
+
+        setNumber(number);
 
     }
 
-    private void setNumber (int number){
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(numberKey, this.number);
+    }
+
+//    @Override
+//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        this.number = savedInstanceState.getInt("number");
+//    }
+
+
+    private void setNumber(int number) {
         TextView textView = findViewById(R.id.textView);
         textView.setText(Integer.toString(number));
     }
 
-    public void subNumber (View view){
+    public void subNumber(View view) {
         TextView textView = findViewById(R.id.textView);
         String textNumber = textView.getText().toString();
-        int number = Integer.parseInt(textNumber);
-        textView.setText(Integer.toString(number - 1));
+        this.number = Integer.parseInt(textNumber);
+        this.number = number - 1;
+        textView.setText(Integer.toString(number));
     }
 
-    public void addNumber (View view){
+    public void addNumber(View view) {
         TextView textView = findViewById(R.id.textView);
         String textNumber = textView.getText().toString();
-        int number = Integer.parseInt(textNumber);
-        textView.setText(Integer.toString(number + 1));
+        this.number = Integer.parseInt(textNumber);
+        this.number = number + 1;
+        textView.setText(Integer.toString(number));
     }
 }
--- app/src/main/java/by/ivpo/homework_ak/homework_1/Hw1Task2Activity.java
+++ app/src/main/java/by/ivpo/homework_ak/homework_1/Hw1Task2Activity.java
@@ -1,6 +1,8 @@
 package by.ivpo.homework_ak.homework_1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,19 +12,33 @@ import by.ivpo.homework_ak.R;
 
 public class Hw1Task2Activity extends AppCompatActivity {
 
+    final String textKey = "text";
+    TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hw1_task2);
+
+        textView = findViewById(R.id.newText);;
+        if (savedInstanceState != null) {
+            this.textView.setText(savedInstanceState.getString(textKey));
+        }
+
     }
 
-    public void goText (View view){
+    public void goText(View view) {
 
         EditText editText = findViewById(R.id.mainText);
-
-        TextView textView = findViewById(R.id.newText);
 
         textView.setText(editText.getText());
 
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(textKey, this.textView.getText().toString());
+    }
+
 }
